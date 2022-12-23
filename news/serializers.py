e7b80b3bd478ba7c949a6bc0e07aef418fd9eeef
korchizhinskiy <korchizhinskiy@gmail.com>
2022-12-23 14:43:55 +0300

    Add fields into serializer
     - Add some field into serializer;
     - Add preview_image field with read_only parameter.

--- news/serializers.py
+++ news/serializers.py
@@ -4,12 +4,17 @@ from news.models import New
 
 class NewSerializer(serializers.ModelSerializer):
     """ModelSerializer of New Model"""
+    preview_image = serializers.ImageField(read_only=True)
 
     class Meta:
         model = New
         fields = (
+            "id",
             "title",
             "content",
             "create_date",
             "upgrade_date",
+            "image",
+            "preview_image",
+            "author",
         )
