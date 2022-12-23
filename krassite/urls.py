108bcf64f987acc247beca1962a59a12cc2078ce
korchizhinskiy <korchizhinskiy@gmail.com>
2022-12-23 14:36:27 +0300

    PEP8 refactoring

--- krassite/urls.py
+++ krassite/urls.py
@@ -6,11 +6,13 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('news.urls')),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-            path('__debug__/', include(debug_toolbar.urls)),
-            ] + urlpatterns
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
