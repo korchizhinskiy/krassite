72ef86fe216c02e70f41100d7a60dd5b839dc52d
korchizhinskiy <korchizhinskiy@gmail.com>
2022-12-23 14:37:34 +0300

    Add adminModel with exclude field and list_display

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from news.models import New
 
-# Register your models here.
+
+@admin.register(New)
+class NewAdmin(admin.ModelAdmin):
+    """New Admin Model"""
+    list_display = (
+        "title",
+        "content",
+        "create_date",
+        "upgrade_date",
+        "image",
+        "preview_image",
+        "author",
+    )
+    exclude = ("preview_image",)
