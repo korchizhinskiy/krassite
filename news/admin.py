f4f7403890a831069d450db440d936731c495522
korchizhinskiy <korchizhinskiy@gmail.com>
2022-12-24 10:58:11 +0300

    Add new fields into admin view
     - Add title into search field with serach helped text;
     - Add title's and content's view with rich by mark_safe utils.

--- news/admin.py
+++ news/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from news.models import New
 from django_summernote.admin import SummernoteModelAdmin
 
@@ -7,16 +8,34 @@ from django_summernote.admin import SummernoteModelAdmin
 class NewAdmin(SummernoteModelAdmin):
     """New Admin Model"""
     list_display = (
-        "title",
-        "content",
+        "get_title_by_html_render",
         "create_date",
         "upgrade_date",
         "image",
         "preview_image",
         "author",
     )
-    exclude = ("preview_image",)
+    search_fields = (
+        "title",
+    )
+    search_help_text = "Введите название публикации"
+    exclude = (
+        "preview_image",
+    )
     summernote_fields = (
         "title",
         "content",
     )
+
+    @admin.display(description="Заголовок")
+    def get_title_by_html_render(self, new):
+        """Get title with html rendering by summernote app"""
+        return mark_safe(new.title)
+
+    @admin.display(description="Контент")
+    def get_content_by_html_render(self, new):
+        """
+        Get content with html rendering by summernote app.
+        Use only with truncate words.
+        """
+        return mark_safe(new.content)
