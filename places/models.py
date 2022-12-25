0e9dea52e74eb0c291ab628d9ce792f90295b2b9
korchizhinskiy <korchizhinskiy@gmail.com>
2022-12-25 09:18:29 +0300

    Add Place model
     - Use only four field: name, lantitude/longitude and rating;
     - In rating use validator for max/min value.

--- places/models.py
+++ places/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Place(models.Model):
+    """Place Model"""
+    name = models.CharField(verbose_name="Название", max_length=100)
+    longitude = models.FloatField(verbose_name="Долгота")
+    latitude = models.FloatField(verbose_name="Широта")
+    rating = models.IntegerField(verbose_name="Рейтинг",
+                                 validators=[
+                                     MaxValueValidator(25),
+                                     MinValueValidator(0)
+                                 ])
+
+    class Meta:
+        verbose_name = "Примечательное место"
+        verbose_name_plural = "Примечательные места"
+
+    def __str__(self):
+        return self.name
