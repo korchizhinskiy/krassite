3c54947bdb28b7b4e2c0d412bac433f9acaf901d
korchizhinskiy <korchizhinskiy@gmail.com>
2022-12-27 18:16:36 +0300

    Add Sender from env

--- krassite/settings.py
+++ krassite/settings.py
@@ -150,9 +150,10 @@ REST_FRAMEWORK = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = "587"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 # REDIS
 REDIS_HOST = "0.0.0.0"
