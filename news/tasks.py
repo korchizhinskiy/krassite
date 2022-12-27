f9357a65d7edcf55c7c2f95c2bb859f42ef2ddaa
korchizhinskiy <korchizhinskiy@gmail.com>
2022-12-27 18:54:59 +0300

    Add task's name and delete constant of email

--- news/tasks.py
+++ news/tasks.py
@@ -2,6 +2,6 @@ from celery import shared_task
 from news.service import send_mail_about_news
 
 
-@shared_task
+@shared_task(name="send_news")
 def send_mail_task():
-    send_mail_about_news(["nosafeyou@mail.ru"])
+    send_mail_about_news()
