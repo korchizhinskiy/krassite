af3ddf3207506e5486a528d6f2461ba582d28ba1
korchizhinskiy <korchizhinskiy@gmail.com>
2022-12-27 15:03:28 +0300

    Use home view in url for send template mail

--- news/urls.py
+++ news/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from news.views import NewViewSet
+from news.views import NewViewSet, home
 
 
 router = routers.SimpleRouter()
 router.register(r"new", NewViewSet, basename="news")
 
 urlpatterns = [
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('send_mail/', home, name="home")
 ]
