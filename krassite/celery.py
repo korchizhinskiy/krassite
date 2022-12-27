5aa34891d37ef609e7fec119ace8da2b5c47747b
korchizhinskiy <korchizhinskiy@gmail.com>
2022-12-27 12:10:46 +0300

    Fix problem | Change new (app) to krassite

--- krassite/celery.py
+++ krassite/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "news.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "krassite.settings")
 
 app = Celery('news')
 app.config_from_object("django.conf:settings", namespace="CELERY")
