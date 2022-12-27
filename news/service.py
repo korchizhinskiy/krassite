7bb7fe30548863435a2587315ed3e519355a2f4e
korchizhinskiy <korchizhinskiy@gmail.com>
2022-12-27 18:54:22 +0300

    Use constance settings in email content

--- news/service.py
+++ news/service.py
@@ -1,11 +1,20 @@
 from django.core.mail import send_mail
 from krassite import settings
+from news.models import Contact
+from constance import config
 
 
-def send_mail_about_news(user_mail: list[str]):
+def send_mail_about_news():
     send_mail(
-        'Вы подписались на рассылку',
-        'Мы пришлем еще сообщения',
-        settings.EMAIL_HOST_USER,
-        recipient_list=user_mail,
+        config.MESSAGE_TOPIC,
+        config.MESSAGE_TEXT,
+        settings.DEFAULT_FROM_EMAIL,
+        get_contacts_for_send_mail(),
+        fail_silently=False
     )
+
+
+def get_contacts_for_send_mail() -> list[str]:
+    queryset = Contact.objects.all()
+    emails = [contact.email for contact in queryset]
+    return emails
