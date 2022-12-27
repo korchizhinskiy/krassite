1f7a561c30aad67a69404216ed64f332f01bdcc9
korchizhinskiy <korchizhinskiy@gmail.com>
2022-12-27 15:04:08 +0300

    Relize home view function with call task and send responce with text

--- news/views.py
+++ news/views.py
@@ -1,6 +1,13 @@
+from django.http import HttpResponse
 from rest_framework import viewsets
+from news import tasks
 from news.serializers import NewSerializer
 from news.models import New
+
+
+def home(request):
+    tasks.send_mail_task.delay()
+    return HttpResponse('<h1>Проверьте почту</h1>')
 
 
 class NewViewSet(viewsets.ModelViewSet):
